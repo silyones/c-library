458663e1eb48fd90a9df44c9ccc00663bc37eb94
silyones <trishasilver12@gmail.com>
2026-06-27 11:22:03 +0530

    used float

---
+++ mylib.c
@@ -0,0 +1,19 @@
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int sub(int a, int b)
+{
+    return a - b;
+}
+
+float div(int a, int b)
+{
+    return (float)a / b;
+}
+
+int mul(int a, int b)
+{
+    return a * b;
+}
